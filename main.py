dde01a5c1afedfd196d02beec1a957d70e30ead1
Qadeer Hussain <qadeeerhussain494@gmail.com>
2023-01-07 18:46:28 +0500

    Just Practice

---
+++ main.py
@@ -0,0 +1,8 @@
+bi = ['AA', 'BB' , 'CC']
+print(bi)
+names = ['strings', 1, True, 1.0, ['one','two'], {'age': 20}, bi]
+print(names)
+print(names[4][1])
+
+message = f"My First Bicycle was a {bi[0].title()}"
+print(message)
